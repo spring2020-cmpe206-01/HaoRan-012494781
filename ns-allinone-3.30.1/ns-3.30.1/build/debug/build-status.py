6abdf0a8c67b95c8881d91e2266dc60665437e5e
HaoRan <chr6538@gmail.com>
2020-02-05 15:34:40 -0800

    init my repo

---
+++ ns-allinone-3.30.1/ns-3.30.1/build/debug/build-status.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+# Programs that are runnable.
+ns3_runnable_programs = ['build/debug/scratch/ns3.30.1-scratch-simulator-debug', 'build/debug/scratch/subdir/ns3.30.1-subdir-debug']
+
+# Scripts that are runnable.
+ns3_runnable_scripts = []
+
